6e02c580b1f8a4371873ff3b38701d94fd8385b3
Matej Svoboda <matej.svoboda@tul.cz>
2026-03-26 15:43:50 +0100

    #8 Pokracovani

---
+++ BooksApp/app/views/books_list.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> -->
+    <title>Knihovna - seznam knih</title>
+</head>
+<body>
+    <header>
+        <h1>Aplikace Knihovna</h1>
+
+        <nav>
+            <ul>
+                <li><a href="/BooksApp/public/index.php">Seznam knih</a></li>
+                <li><a href="/BooksApp/public/index.php?url=book/create">Přidat novou knihu</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <h2>Dostupné knihy</h2>
+
+        <!-- Zde se bude zobrazovat seznam knih načtených z databáze -->
+        <!-- V další fázi se zde přidá PHP kód pro načítání a zobrazení knih -->
+        <p>Zde bude seznam knih z databáze.</p>
+    </main>
+    
+    <footer>
+        <p>&copy; WA 2026 - Výukový projekt</p>
+    </footer>
+</body>
+</html>
---
+++ BooksApp/public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Zapnutí zobrazení chyb pro vývojové prostředí
+// Tyto direktivy umožňují zobrazit všechny chyby, což je užitečné pro ladění během vývoje.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Autoloading tříd pomocí Composeru
+// Tento řádek načítá všechny třídy definované v projektu pomocí Composer
+require_once '../core/App.php';
+
+// Vytvoření instance třídy App, která je zodpovědná za zpracování požadavků a směrování
+$app = new App();
+
+?>
